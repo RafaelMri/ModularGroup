4bd8edf19f7d207252e0e1ebe095fb25bafc7d5a
Thomas Ponweiser <thomas.ponweiser@gmail.com>
2013-06-23 15:31:49 +0000

    * Queue.m: Removed type checks again.

--- nb/Queue.m
+++ nb/Queue.m
@@ -17,31 +17,26 @@ EmptyQ::usage =
   "EmptyQ[queue] tests if queue is empty.";
 
 FifoQueue::usage = 
-  "First-in-first-out queue. " <>
-  "Example: myFifo = New[FifoQueue[t]] " <>
-  "constructs a fifo queue for element type t.";
+  "First-in-first-out queue. ";
 
 LifoQueue::usage =
-  "Last-in-first-out queue (a.k.a. stack). " <>
-  "Example: myStack = New[LifoQueue[t]] " <>
-  "constructs a lifo queue for element type t.";
+  "Last-in-first-out queue (a.k.a. stack).";
 
 PriorityQueue::usage =
   "Priority queue (a.k.a heap). " <>
-  "Example: myHeap = New[PriorityQueue[t, p]] " <>
-  "constructs a priority queue for element type t " <>
-  "with p as ordering function.";
+  "Example: myHeap = New[PriorityQueue, p] " <>
+  "constructs a priority queue using p as ordering function.";
 
 Enumerator::usage = 
   "e = New[Enumerator, queue, f] constructs an Enumerator " <> 
   "with transition function f. " <>
-  "f should take one argument of the queue's element type t " <> 
-  "and return a list of new elements of type t to be enqueued.";
+  "f should take one argument corresponding to the queue's element type " <> 
+  "and return a list of elements of same type to be enqueued.";
 
 GetNext::usage =
   "GetNext[e] returns the next element of the Enumerator e. " <>
   "The next element n is taken from the top of the Enumerator's queue " <>
-  "and all elements f[n] are added to the queue.";
+  "and all elements returned by f[n] are added to the queue.";
 
 HasNext::usage = 
   "HasNext[e] tests if the Enumerator e has a next element. " <>
@@ -52,34 +47,19 @@ Begin["`Private`"];
 (* -------------------------------------------------------------- Class Queue *)
 NewAbstractClass[Queue];
 
-Queue[elemType_?(InstanceQ[Class])] := Queue[elemType] = 
-Module[{classObj},
-  NewAbstractClass[classObj];
-  Init[classObj, obj_] ^:= (
-    obj /: InstanceQ[Queue][obj] = True;
-  );
-  classObj
-];
+Init[Queue, obj_] ^:= Null;
 
 (* ---------------------------------------------------------- Class FifoQueue *)
 NewClass[FifoQueue];
 
-FifoQueue[elemType_?(InstanceQ[Class])] := FifoQueue[elemType] = 
-Module[{classObj},
-  NewClass[classObj];
-  
-  Init[classObj, obj_] ^:= (
-    Super[Queue[elemType], obj];
-    obj /: InstanceQ[FifoQueue][obj] = True;
-    Contents@obj ^= {};
-  );
+Init[FifoQueue, obj_] ^:= (
+  Super[Queue, obj];
+  Contents@obj ^= {};
+);
 
-  Enqueue[queue_?(InstanceQ[classObj]), elem_?(InstanceQ[elemType])] := (
-    Contents@queue ^= Append[Contents@queue, elem];
-  );
-
-  classObj
-];
+Enqueue[queue_?(InstanceQ[FifoQueue]), elem_] := (
+  Contents@queue ^= Append[Contents@queue, elem];
+);
 
 EmptyQ[queue_?(InstanceQ[FifoQueue])] := (
   Length@Contents@queue == 0
@@ -95,21 +75,14 @@ Module[{elem},
 (* ---------------------------------------------------------- Class LifoQueue *)
 NewClass[LifoQueue];
 
-LifoQueue[elemType_?(InstanceQ[Class])] := LifoQueue[elemType] = Module[{classObj},
-  NewClass[classObj];
-  
-  Init[classObj, obj_] ^:= (
-    Super[Queue[elemType], obj];
-    obj /: InstanceQ[LifoQueue][obj] = True;
-    Contents@obj ^= {};
-  );
+Init[LifoQueue, obj_] ^:= (
+  Super[Queue, obj];
+  Contents@obj ^= {};
+);
 
-  Enqueue[queue_?(InstanceQ[classObj]), elem_?(InstanceQ[elemType])] := (
-    Contents@queue ^= {elem, Contents@queue};
-  );
-
-  classObj
-];
+Enqueue[queue_?(InstanceQ[LifoQueue]), elem_] := (
+  Contents@queue ^= {elem, Contents@queue};
+);
 
 EmptyQ[queue_?(InstanceQ[LifoQueue])] := (
   Length@Contents@queue == 0
@@ -125,28 +98,20 @@ Module[{elem},
 (* ------------------------------------------------------ Class PriorityQueue *)
 NewClass[PriorityQueue];
 
-PriorityQueue[elemType_?(InstanceQ[Class])] := PriorityQueue[elemType] = 
-Module[{classObj},
-  NewClass[classObj];
-    
-  Init[classObj, obj_, orderingFunction_] ^:= (
-    Super[Queue[elemType], obj];
-    obj /: InstanceQ[PriorityQueue][obj] = True;
-    OrderingFunction[obj] ^= orderingFunction;
-    Contents[obj] ^= Table[Null, {i,16}];
-    Size[obj] ^= 0;
-  );
+Init[PriorityQueue, obj_, orderingFunction_] ^:= (
+  Super[Queue, obj];
+  OrderingFunction[obj] ^= orderingFunction;
+  Contents[obj] ^= Table[Null, {i,16}];
+  Size[obj] ^= 0;
+);
 
-  Enqueue[queue_?(InstanceQ[classObj]), elem_?(InstanceQ[elemType])] := 
-  Module[{contents},
-    contents = Grow[queue];
-    Size@queue ^= Size@queue + 1;
-    contents[[Size@queue]] = elem;
-    Contents@queue ^= contents;
-    Contents@queue ^= HeapUp[queue];
-  ];
-
-  classObj
+Enqueue[queue_?(InstanceQ[PriorityQueue]), elem_] := 
+Module[{contents},
+  contents = Grow[queue];
+  Size@queue ^= Size@queue + 1;
+  contents[[Size@queue]] = elem;
+  Contents@queue ^= contents;
+  Contents@queue ^= HeapUp[queue];
 ];
 
 Grow[queue_] := Module[{contents, capacity},
@@ -232,5 +197,9 @@ Module[{q, next},
 ];
 
 End[];
+
+Protect[Queue, Enqueue, Dequeue, EmptyQ];
+Protect[FifoQueue, LifoQueue, PriorityQueue];
+Protect[Enumerator, GetNext, HasNext];
 
 EndPackage[];
