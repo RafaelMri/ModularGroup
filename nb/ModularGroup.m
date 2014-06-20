b37722844e79149b4c9a9a5e336507a397e9c125
Thomas Ponweiser <thomas.ponweiser@gmail.com>
2014-06-20 11:10:55 +0000

    inserted MIT license, merged packages into ModularGroup.m

--- nb/ModularGroup.m
+++ nb/ModularGroup.m
@@ -1,6 +1,411 @@
 (* ::Package:: *)
 
-BeginPackage["ModularGroup`", {"OOP`", "Queue`"}];
+(*
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2014 Thomas Ponweiser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *)
+
+
+(* 
+ * Helper package for "object-oriented programming" (OOP) needed by main 
+ * package ModularGroup.
+ *)
+BeginPackage["ModularGroup`OOP`"];
+
+Unprotect[Class, NewClass, New, Init, Super, InstanceQ, Info];
+Unprotect[Iterator, ListIterator, HasNext, GetNext];
+
+(* ---------------------------------------------------------- Public Elements *)
+
+Class::usage = 
+  "Class is a reserved symbol identifying the type of a class.";
+NewClass::usage = 
+  "NewClass[c] declares the symbol c to be a class.";
+NewAbstractClass::usage =
+  "NewAbstractClass[c] declares the symbol c to be an abstract class.";
+
+AbstractQ::usage =
+  "AbstractQ[c] returns True if c is an abstract class.";
+
+New::usage = StringJoin[
+  "New[c, args...] instantiates an objtect of class c, ",
+  "passing the given arguments to its constructor."
+];
+New::notaclass = "`1` is not a class.";
+New::abstractinst = "`1` is an abstract class and cannot be instantiated.";
+
+Init::usage = StringJoin[
+  "Init[c, o, args...] defines the constructor of a class c ",
+  "and initializes the new object o. ",
+  "Never call directly, use o = New[c, args...] instead."
+];
+Init::undef = StringJoin[
+  "Constructor for class `1` and arguments `2` not defined. ",
+  "Make sure you have defined Init[`1`, obj_, args___] ^:= ..."
+];
+
+Super::usage = StringJoin[
+  "Super[b, o, args...] calls the constructor of a base-class b ",
+  "for a new object o. ",
+  "To be used within the Init function of a class c which inherits from b."
+];
+
+InstanceQ::usage = StringJoin[
+  "InstanceQ[c][o] returns True, if the object o is instance of class c."
+];
+
+Info::usage = StringJoin[
+  "Info[o] displays data associated with the object o."
+];
+
+Iterator::usage = StringJoin[
+  "Iterator is an abstract base class for objects o providing methods ",
+  "HasNext[o] and GetNext[o]."
+];
+
+HasNext::usage =
+  "HasNext[o] tests if the Iterator o has a next element.";
+
+GetNext::usage =
+  "GetNext[o] returns the next value of the Iterator o.";
+
+ListIterator::usage =
+  "New[ListIterator, l] creates an iterator for the objects in the list l.";
+
+Begin["`Private`"];
+
+NewClass[class_] := (
+  class /: InstanceQ[Class][class] = True;
+  class /: Init[class, obj_, args___] := (
+    Message[Init::undef, class, {args}];
+    $Failed
+  );
+);
+NewAbstractClass[class_] := (
+  NewClass[class];
+  AbstractQ[class] ^= True;
+);
+
+New[class_?(InstanceQ[Class]), args___] := (
+  If[TrueQ@AbstractQ@class, (
+    Message[New::abstractclass, class];
+    $Failed
+  ), (
+    Module[{obj = Unique[class]},
+      Super[class, obj, args];
+      obj
+    ]
+  )]
+);
+
+New[notaclass_, args___] := (
+  Message[New::notaclass, notaclass];
+  $Failed
+);
+
+Super[class_?(InstanceQ[Class]), obj_, args___] := (
+  Init[class, obj, args];
+  obj /: InstanceQ[class][obj] = True;
+);
+
+Info[obj_] := Information@Evaluate@obj;
+
+NewAbstractClass[Iterator];
+Init[Iterator, obj_] ^:= Null;
+
+NewClass[ListIterator];
+Init[ListIterator, obj_, list_] ^:= (
+  Super[Iterator, obj];
+  Pos[obj] ^= 1;
+  HasNext[obj] ^:= Pos[obj] <= Length[list];
+  GetNext[obj] ^:= Module[{next}, 
+    next = list[[Pos[obj]]]; 
+    Pos[obj] ^= Pos[obj]+1;
+    next
+  ];
+);
+
+End[];
+
+Protect[Class, NewClass, New, Init, Super, InstanceQ, Info];
+Protect[Iterator, ListIterator, HasNext, GetNext];
+
+EndPackage[];
+
+
+(*
+ * Helper package with generic queue data structures and enumeration 
+ * algorithms used by main package ModularGroup.
+ *)
+BeginPackage["ModularGroup`Queue`", {"ModularGroup`OOP`"}];
+
+Unprotect[Queue, Enqueue, Dequeue, EmptyQ];
+Unprotect[FifoQueue, LifoQueue, PriorityQueue];
+Unprotect[SearchIteartor, BFSIterator, DFSIterator, PFSIterator];
+
+(* ---------------------------------------------------------- Public Elements *)
+
+Queue::usage = 
+  "Queue is an abstract base class for queues and cannot be instantiated.";
+
+Enqueue::usage =
+  "Enqueue[q, e] enqueues the element e to the queue q.";
+
+Dequeue::usage =
+  "Dequeue[q] returns the element at the top of the queue q.";
+
+EmptyQ::usage =
+  "EmptyQ[q] tests if queue q is empty.";
+
+FifoQueue::usage = 
+  "New[FifoQueue] constructs an empty first-in-first-out queue.";
+
+LifoQueue::usage = StringJoin[
+  "New[LifoQueue] constructs an empty last-in-first-out queue, ",
+  "also known as stack."
+];
+
+PriorityQueue::usage = StringJoin[
+  "New[PriorityQueue, p] constructs an empty priority queue ",
+  "with p as the ordering predicate."
+];
+
+SearchIterator::usage = StringJoin[
+  "it = New[SearchIterator, q, f, p] constructs an iterator over a sequence ",
+  "which is defined by the initial contents of the Queue q, ",
+  "a transition function f and a predicate p.",
+  "\nHasNext[it] returns True as long as q contains elements satisfying p.",
+  "\nGetNext[it] returns the first element t of q which satisfies p ",
+  "and enqueues all elements f[t]={a,b,c,...} to q."
+];
+
+BFSIterator::usage = StringJoin[
+  "New[BFSIterator, s, f, p] constructs an iterator ",
+  "for performing a breadth-first-search ",
+  "starting with the element s. For any element e, ",
+  "f[e] should return a (possibly empty) list of elements to be visited next."
+];
+
+DFSIterator::usage = StringJoin[
+  "New[DFSIterator, s, f] constructs an iterator ",
+  "for performing a depth-first-search ",
+  "starting with the element s. For any element e, ",
+  "f[e] should return a (possibly empty) list of elements to be visited next."
+];
+
+PFSIterator::usage = StringJoin[
+  "New[PFSIterator, s, f, o, p] constructs an iterator ",
+  "for performing a priority-first-search ",
+  "starting with the element s and using the ordering predicate o. ",
+  "For any element e, ",
+  "f[e] should return a (possibly empty) list of elements to be visited next."
+];
+
+Begin["`Private`"];
+
+(* -------------------------------------------------------------- Class Queue *)
+NewAbstractClass[Queue];
+
+Init[Queue, obj_] ^:= Null;
+
+(* ---------------------------------------------------------- Class FifoQueue *)
+NewClass[FifoQueue];
+
+Init[FifoQueue, obj_] ^:= (
+  Super[Queue, obj];
+  Contents@obj ^= {};
+);
+
+Enqueue[queue_?(InstanceQ[FifoQueue]), elem_] := (
+  Contents@queue ^= Append[Contents@queue, elem];
+);
+
+EmptyQ[queue_?(InstanceQ[FifoQueue])] := (
+  Length@Contents@queue == 0
+);
+
+Dequeue[queue_?(InstanceQ[FifoQueue])] := 
+Module[{elem},
+  elem = First@Contents@queue;
+  Contents@queue ^= Rest@Contents@queue;
+  elem
+];
+
+(* ---------------------------------------------------------- Class LifoQueue *)
+NewClass[LifoQueue];
+
+Init[LifoQueue, obj_] ^:= (
+  Super[Queue, obj];
+  Contents@obj ^= {};
+);
+
+Enqueue[queue_?(InstanceQ[LifoQueue]), elem_] := (
+  Contents@queue ^= {elem, Contents@queue};
+);
+
+EmptyQ[queue_?(InstanceQ[LifoQueue])] := (
+  Length@Contents@queue == 0
+);
+
+Dequeue[queue_?(InstanceQ[LifoQueue])] := 
+Module[{elem},
+  elem = Contents[queue][[1]];
+  Contents@queue ^= Contents[queue][[2]];
+  elem
+];
+
+(* ------------------------------------------------------ Class PriorityQueue *)
+NewClass[PriorityQueue];
+
+Init[PriorityQueue, obj_, orderingFunction_] ^:= (
+  Super[Queue, obj];
+  OrderingFunction[obj] ^= orderingFunction;
+  Contents[obj] ^= Table[Null, {i,16}];
+  Size[obj] ^= 0;
+);
+
+Enqueue[queue_?(InstanceQ[PriorityQueue]), elem_] := 
+Module[{contents},
+  contents = Grow[queue];
+  Size@queue ^= Size@queue + 1;
+  contents[[Size@queue]] = elem;
+  Contents@queue ^= contents;
+  Contents@queue ^= HeapUp[queue];
+];
+
+Grow[queue_] := Module[{contents, capacity},
+  contents = Contents@queue;
+  capacity = Length@contents;
+  If[Size@queue >= capacity,
+    contents = Join[contents, Table[Null, {i, capacity}]];
+  ]; 
+  contents
+];
+
+HeapUp[queue_] := 
+Module[{contents, orderingFunction, i, p},
+  contents = Contents@queue;
+  orderingFunction = OrderingFunction@queue;
+  i = Size@queue;
+  p = BitShiftRight[i, 1];
+  While[p > 0 && !orderingFunction[contents[[p]], contents[[i]]],
+    {contents[[p]], contents[[i]]} = {contents[[i]], contents[[p]]};
+    i = p; 
+    p = BitShiftRight[p, 1];
+  ]; 
+  contents
+];
+
+EmptyQ[queue_?(InstanceQ[PriorityQueue])] := (
+  Size@queue == 0
+);
+
+Dequeue[queue_?(InstanceQ[PriorityQueue])] := 
+Module[{contents, top},
+  contents = Contents@queue;
+  top = contents[[1]];
+  contents[[1]] = contents[[Size[queue]]];
+  Size[queue] ^= Size[queue] - 1;
+  Contents@queue ^= contents;
+  Contents@queue ^= HeapDown[queue];
+  top
+];
+
+HeapDown[queue_] :=
+Module[{contents, orderingFunction, size, i, c},
+  contents = Contents@queue;
+  orderingFunction = OrderingFunction@queue;
+  size = Size@queue;
+  i = 1;
+  c = SmallerChild[contents, queue, i];
+  While[c <= size && !orderingFunction[contents[[i]], contents[[c]]],
+    {contents[[i]], contents[[c]]} = {contents[[c]], contents[[i]]};
+    i = c;
+    c = SmallerChild[contents, queue, i];
+  ];
+  contents
+];
+
+SmallerChild[contents_, queue_, i_] := 
+Module[{c},
+  c = BitShiftLeft[i, 1];
+  If[c+1 <= Size@queue && OrderingFunction[queue][contents[[c+1]], contents[[c]]],
+    c++;
+  ];
+  c
+];
+
+(* --------------------------------------------------------- Class Enumerator *)
+NewClass[SearchIterator];
+Init[SearchIterator, obj_, queue_?(InstanceQ[Queue]), f_, p_:(True&)] ^:= 
+Module[{current, next, hasNext, SetNext},
+  SetNext := (
+    hasNext = False;
+    While[!EmptyQ[queue] && !(hasNext = p[next = Dequeue[queue]])];
+  );
+  SetNext;
+  HasNext[obj] ^:= hasNext;
+  GetNext[obj] ^:= (
+    current = next; 
+    Enqueue[queue, #]& /@ f[current];
+    SetNext;
+    current
+  );
+];
+
+NewClass[BFSIterator];
+Init[BFSIterator, obj_, s_, f_, p_:(True&)] ^:= Module[{fifo},
+  fifo = New[FifoQueue];
+  If[Head[s] === List, Enqueue[fifo, #]& /@ s, Enqueue[fifo,s]];
+  Super[SearchIterator, obj, fifo, f, p];
+];
+
+NewClass[DFSIterator];
+Init[DFSIterator, obj_, s_, f_, p_:(True&)] ^:= Module[{stack},
+  stack = New[LifoQueue];
+  If[Head[s] === List, Enqueue[stack, #]& /@ s, Enqueue[stack,s]];
+  Super[SearchIterator, obj, stack, f, p];
+];
+
+NewClass[PFSIterator];
+Init[PFSIterator, obj_, s_, f_, o_, p_:(True&)] ^:= Module[{heap},
+  heap = New[PriorityQueue, o];
+  If[Head[s] === List, Enqueue[heap, #]& /@ s, Enqueue[heap,s]];
+  Super[SearchIterator, obj, heap, f, p];
+];
+
+End[];
+
+Protect[Queue, Enqueue, Dequeue, EmptyQ];
+Protect[FifoQueue, LifoQueue, PriorityQueue];
+Protect[SearchIteartor, BFSIterator, DFSIterator, PFSIterator];
+
+EndPackage[];
+
+
+(*
+ * The definitions for the main package ModularGroup start here.
+ *)
+BeginPackage["ModularGroup`", {"ModularGroup`OOP`", "ModularGroup`Queue`"}];
 
 
 (* ---------------------------------------------------------- Public Elements *)
